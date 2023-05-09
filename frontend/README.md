dea4b5cf5e36b52f8f19b135d02a5e96bcbf497b
Anubhav Pathak <anubhavpathak99@gmail.com>
2023-05-09 23:23:25 +0530

    :tada: Added FrontEnd

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+# MIS Law Project - FrontEnd
---
+++ frontend/src/pages/dashboard.tsx
@@ -0,0 +1,13 @@
+// ${baseURL}/dashboard
+
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <main className='flex flex-col min-h-screen justify-center items-center'>
+    ...
+    </main>
+  )
+}
+
+export default Dashboard
