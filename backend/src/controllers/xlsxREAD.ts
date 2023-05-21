2130cf6e2ec217b738acbc6b7d27077c868de74f
alvinbengeorge <alvinallen333@gmail.com>
2023-05-21 14:00:14 +0530

    Completed pagination and limit

--- backend/src/controllers/xlsxREAD.ts
+++ backend/src/controllers/xlsxREAD.ts
@@ -4,12 +4,15 @@ import { Response, Request } from 'express';
 export function read(req: Request, res: Response) {
     const company: string = req.params.company;
     const workbook: xlsx.WorkBook = xlsx.readFile(`./src/data/files/data.xlsx`);
+    if (!workbook.SheetNames.includes(company)) {
+        return res.status(404).send({ message: 'Company not found' });
+    }
     const worksheet: xlsx.WorkSheet = workbook.Sheets[company];
-    const data: any[] = xlsx.utils.sheet_to_json(worksheet, { raw: true });
     const page: number = req.query.page ? parseInt(req.query.page as string) : 1;
     const limit: number = req.query.limit ? parseInt(req.query.limit as string) : 15;
     const startIndex: number = (page - 1) * limit;
     const endIndex: number = page * limit;
+    const data: any[] = xlsx.utils.sheet_to_json(worksheet);
     const results: any[] = data.slice(startIndex, endIndex);
     res.status(200).send({ 
         data: results, 
