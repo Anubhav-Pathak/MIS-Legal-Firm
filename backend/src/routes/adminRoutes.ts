d17111d8c657828d7bb46615eab5182e74cbf8cd
alvinbengeorge <alvinallen333@gmail.com>
2023-06-07 14:41:51 +0530

    Added search and filter

--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { postRead, upload } from "../controllers/xlsx";
+import { postRead, upload, getfilterBy, postSearch } from "../controllers/xlsx";
 
 const router: Router = Router();
 
 router.post("/read", postRead);
 router.put("/upload", upload);
+router.get("/filter", getfilterBy);
+router.post("/filter", postSearch);
 
 export default router;
