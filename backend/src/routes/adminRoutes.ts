9908e21ed12efc5044b9fe0a41b6d13512ed88d4
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2023-07-22 23:39:01 +0530

    Merge branch 'main' of github.com:alvinbengeorge/MIS-Legal-Firm

--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { postAddUser, deleteClient, editClient, getUsers } from "../controllers/admin.controller";
+import isAdmin from "../middlewares/isAdmin";
 
 const router: Router = Router();
 
-router.post("/add-user", postAddUser);
-router.delete("/:clientName", deleteClient); //should change to id
-router.put("/:clientName", editClient); //should change to id
+router.post("/add-user", isAdmin, postAddUser);
+router.delete("/:clientName", isAdmin, deleteClient); //should change to id
+router.put("/:clientName", isAdmin, editClient); //should change to id
 
-router.get("/companies", getUsers);
+router.get("/companies", isAdmin, getUsers);
 
 export default router;
