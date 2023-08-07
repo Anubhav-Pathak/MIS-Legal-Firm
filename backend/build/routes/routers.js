109a250b55c3f68499a2bae69c1a49aa2e0ad764
Anubhav Pathak <anubhavpathak99@gmail.com>
2023-08-07 07:52:39 +0530

    Fixing Render error

--- backend/build/routes/routers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const xlsxREAD_js_1 = require("../controllers/xlsxREAD.js");
-const router = (0, express_1.Router)();
-router.get("/:filename", xlsxREAD_js_1.read);
-exports.default = router;
--- backend/build/routes/xlsxRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const xlsx_js_1 = require("../controllers/xlsx.js");
-const router = (0, express_1.Router)();
-router.get("/read/", xlsx_js_1.read);
-router.post("/upload", xlsx_js_1.upload);
-exports.default = router;
