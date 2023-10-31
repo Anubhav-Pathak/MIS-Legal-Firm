b3b0c2d8f64c3f46ad4dc17d4f7eaa0e746dc76c
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2023-10-31 21:40:29 +0530

    removing build

--- backend/build/routes/pdfRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const pdf_controller_1 = require("../controllers/pdf.controller");
-const router = (0, express_1.Router)();
-router.get("/templates", pdf_controller_1.getTemplates);
-router.get("/:filename", pdf_controller_1.getFile);
-router.post("/upload", pdf_controller_1.uploadFile);
-exports.default = router;
